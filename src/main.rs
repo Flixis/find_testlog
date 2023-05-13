3c094c367875cd7b37985695e93647a60bb3fe24
Tariq <tariq9853@gmail.com>
2023-05-13 22:14:39 +0200

    Switched to confy for configuring config

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use std::fs;
+use serde::{Serialize, Deserialize};
 use clap::Parser;
 
-
-/*
-
-find_testlog -pn 6001-xxxx-xxxx -sn xxx-xxx-xxx
-
- */
-
-
- #[derive(Parser)]
- #[command(author, version, about, long_about = None)]
- struct Cli {
+#[derive(Parser, Debug, Serialize, Deserialize)]
+struct MyConfig {
     /// Optional name to operate on
     #[arg(short, long)]
     drive: Option<String>,
@@ -25,62 +16,21 @@ find_testlog -pn 6001-xxxx-xxxx -sn xxx-xxx-xxx
 
     /// Required SN if not defined it will be pulled from config
     sn:Option<String>,
- }
-
-
-
-
-fn main() {
-
-    let cli_parse = Cli::parse();
-
-
-    let parsed_json = parse_json("find_testlog_config.json".to_string());
-
-    parse_settings(&parsed_json, cli_parse);
-
-    println!("{}", &parsed_json);
 }
 
 
-
-
-fn parse_json(file_path:String) -> serde_json::value::Value {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let json_data = fs::read_to_string(&file_path).expect("couldn't read");
-
-    // Parse the string of data into serde_json::Value.
-    let json_serialized: Value = serde_json::from_str(&json_data).unwrap();
-
-    return json_serialized;
+/// `MyConfig` implements `Default`
+impl ::std::default::Default for MyConfig {
+    fn default() -> Self { Self { 
+        drive: Some("k".to_string()), 
+        location: Some("k".to_string()),
+        pn: Some("".to_string()),
+        sn: Some("".to_string()),
+    } }
 }
 
-
-fn parse_settings(json_serialized:&serde_json::value::Value, cli_arguments:Cli){
-
-    let Some(drive_cli) = cli_arguments.drive;
-    
-   
-    match (cli_arguments.drive, json_serialized.get("drive")) {
-        (None, _) =>{
-            println!("Couldn't find drive in config or as an argument.");
-        }
-        (_, Some(drive)) => {
-            println!("Using drive: {}", drive);
-        }
-        (_, None) => {
-            println!("Using drive: {}", );
-        }
-    }
-
-    
-    
-}
-
-
-
-//Helps me figure out what types are floating around :)
-#[warn(dead_code)]
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
+fn main() -> Result<(), confy::ConfyError> {
+    let cfg: MyConfig = confy::load("my-app-name", None)?;
+    dbg!(cfg);
+    Ok(())
 }
