2ac80f2abfc50dc1f4aeb6bf88fd8b30c7415c7b
Tariq <tariq9853@gmail.com>
2023-05-22 22:28:56 +0200

    added checks for args and changed default-settings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::println;
+use std::{println};
 use serde::{Serialize, Deserialize};
 use clap::Parser;
 use colored::*;
@@ -7,32 +7,34 @@ use colored::*;
 
 find_testlog <SN> <PN>
 
+returns: list of 5 latest testresults
+
  */
 
 #[derive(Parser, Debug, Serialize, Deserialize)]
 #[command(author, version, about, long_about = None)]
-struct CliAndConfig {
+pub struct CliAndConfig {
     /// Sets a custom drive
     #[arg(short, long)]
-    drive: Option<String>,
+    pub drive: Option<String>,
 
     /// Sets a custom directory
     #[arg(short, long)]
-    location: Option<String>,
+    pub location: Option<String>,
 
     /// PN if not defined it will be pulled from config
-    pn:Option<String>,
+    pub pn:Option<String>,
 
     /// SN if not defined it will be pulled from config
-    sn:Option<String>,
+    pub sn:Option<String>,
 }
 
 
 /// `CliAndConfig` implements `Default`
 impl Default for CliAndConfig {
     fn default() -> Self { Self { 
-        drive: Some("k".to_string()), 
-        location: Some("k".to_string()),
+        drive: Some("d:".to_string()), //TODO: set correct drive letter
+        location: Some("testlogs".to_string()), //TODO: set correct default folder
         pn: Some("".to_string()),
         sn: Some("".to_string()),
     } }
@@ -41,9 +43,9 @@ impl Default for CliAndConfig {
 
 fn handle_settings(app_name:&str) -> Result<(), confy::ConfyError> {
     let _cfg: CliAndConfig = confy::load(app_name, None)?;
-    // dbg!(cfg);
     Ok(())
 }
+
 
 fn main(){
 
@@ -54,7 +56,16 @@ fn main(){
     if std::env::args().len() <= 1 {
         let file = confy::get_configuration_file_path("confy_simple_app", None).unwrap();
         println!("{} {:#?}", "Configuration file is located at:".red().bold(), file);
+        
     }
     
     handle_settings(&app_name).unwrap();
+
+    if let Some(pn) = _cli_parse.pn.as_deref() {
+        println!("Value for PN: {pn}");
+    }
+
+    if let Some(sn) = _cli_parse.sn.as_deref() {
+        println!("Value for SN: {sn}");
+    }
 }
