3fe48ab9269bcedd7c73e6e06eeefee41356115f
Tariq <tariq9853@gmail.com>
2023-09-06 23:13:01 +0200

    got this testing fase working

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
 const { invoke } = window.__TAURI__.tauri;
 
 let greetInputEl;
+let yearWeekEl;
 let greetMsgEl;
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+async function getvaluefromtextbox() {
+  greetMsgEl.textContent = await invoke("greet", { name: yearWeekEl.value });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
+  yearWeekEl = document.querySelector("#year_week");
   greetMsgEl = document.querySelector("#greet-msg");
+
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
+  });
+
+  // Assuming you have a form with id="year-week-form"
+  document.querySelector("#year-week-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    getvaluefromtextbox();
   });
 });
--- src/sortable.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // Make the table rows sortable
-    $("#sortable-table tbody").sortable();
-    $("#sortable-table tbody").disableSelection();
-  });
-  
