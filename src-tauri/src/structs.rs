09a590389b6a9355d1c99eff9e6218087d26b97b
Tariq <tariq9853@gmail.com>
2023-09-25 22:54:03 +0200

    add: error handling for getting logfile

--- src-tauri/src/structs.rs
+++ src-tauri/src/structs.rs
@@ -1,4 +1,3 @@
-use clap::Parser;
 use confy::ConfyError;
 
 // Struct for application configuration
@@ -10,6 +9,7 @@ pub struct AppConfig {
     pub drive_letter: String,
     pub folder_location: String,
     pub test_env: String,
+    pub open_log: bool,
 }
 
 // Default values for AppConfig
@@ -22,6 +22,7 @@ impl Default for AppConfig {
             drive_letter: String::from("Q:"),
             folder_location: String::from("TestLogs"),
             test_env: String::from("PTF"),
+            open_log: false,
         }
     }
 }
@@ -42,41 +43,4 @@ impl AppConfig {
     pub fn default_values() -> AppConfig {
         Self::load().unwrap_or_default()
     }
-}
-
-// Struct for command line arguments
-#[derive(Parser, Clone)]
-#[command(author, version, about, long_about = None)]
-pub struct Cli {
-    #[clap(short, long)]
-    ///Product Number, Example: 6107-2100-6301.
-    pub pn: Option<String>,
-
-    #[clap(short, long)]
-    ///Serial Number, Example: 22-39-A2Y-15I
-    pub sn: Option<String>,
-
-    #[clap(short, long)]
-    ///Year Week, Example: 2023-W51, Defaults: Searches all year-week folders.
-    pub year_week: Option<String>,
-
-    #[clap(short, long)]
-    ///Test environment, Default: PTF
-    pub test_env: Option<String>,
-
-    #[clap(short, long)]
-    ///Will automatically open the resulting log files, WARNING OPENS ALL OF THEM.
-    pub open_log: bool,
-
-    #[clap(short, long)]
-    ///Drive letter, Default Q:
-    pub drive_letter: Option<String>,
-
-    #[clap(short, long)]
-    ///Folder location, Default: TestLogs.
-    pub folder_location: Option<String>,
-
-    #[clap(short, long)]
-    ///If passed, Returns config location
-    pub get_config_location: bool,
 }
