12191a6aaf285a183e283901fa96a3f2601231ff
Tariq <tariq9853@gmail.com>
2023-10-06 19:45:24 +0200

    Add: Comments for .py module

--- folder_structure_generator/main.py
+++ folder_structure_generator/main.py
@@ -27,26 +27,61 @@ def generate_random_string():
   random_sn = generate_random_sn()
   return f"{date}_{time}_CLNT{random_int}_group_0_{random_sn}.log"
 
+# Generates a random folder structure in the format <drive>:<folder>/<pn_formatted>/<year>-W<week_str>/<test_env>/<log_file_name>
 def generate_random_folder_structure(drive, folder, pn_min, pn_max, year_min, year_max, week_min, week_max, test_env_list):
-    pn = random.randint(pn_min, pn_max)
-    pn_str = str(pn)
-    pn_formatted = f"{pn_str[:4]}-{pn_str[4:8]}-{pn_str[8:12]}"
-    
-    year = random.randint(year_min, year_max)
-    week = random.randint(week_min, week_max)
-    
-    week_str = str(week).zfill(2)
-    
-    test_env = random.choice(test_env_list)
-    
-    log_file_name = generate_random_string() #replace this with "20231006_194142_CLNT2228_group_0_41-82-SBV-LBK" or some shit if you wanna have the same sn in different folders.
-    log_file_path = os.path.join(drive, folder, pn_formatted, f"{year}-W{week_str}", test_env, log_file_name)
-    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
-        
-    with open(log_file_path, "w") as f:
-        f.write("This is a test log file." + log_file_name)
-    
-    return log_file_path
+  """
+  Generates a random folder structure in the format:
+
+  <drive>:<folder>/<pn_formatted>/<year>-W<week_str>/<test_env>/<log_file_name>
+
+  Args:
+    drive: The drive letter to generate the folder structure on.
+    folder: The folder name to generate the folder structure in.
+    pn_min: The minimum PN value to use.
+    pn_max: The maximum PN value to use.
+    year_min: The minimum year value to use.
+    year_max: The maximum year value to use.
+    week_min: The minimum week value to use.
+    week_max: The maximum week value to use.
+    test_env_list: A list of test environments to choose from.
+
+  Returns:
+    The path to the generated log file.
+  """
+
+  # Generate a random PN value.
+  pn = random.randint(pn_min, pn_max)
+  # Format the PN value as a string.
+  pn_str = str(pn)
+  # Pad the PN string to 12 characters.
+  pn_formatted = f"{pn_str[:4]}-{pn_str[4:8]}-{pn_str[8:12]}"
+
+  # Generate a random year value.
+  year = random.randint(year_min, year_max)
+  # Generate a random week value.
+  week = random.randint(week_min, week_max)
+
+  # Format the week value as a string and pad it to 2 characters.
+  week_str = str(week).zfill(2)
+
+  # Choose a random test environment.
+  test_env = random.choice(test_env_list)
+
+  # Generate a random log file name.
+  log_file_name = generate_random_string()
+
+  # Construct the path to the log file.
+  log_file_path = os.path.join(drive, folder, pn_formatted, f"{year}-W{week_str}", test_env, log_file_name)
+
+  # Create the directories if they don't already exist.
+  os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+
+  # Create the log file.
+  with open(log_file_path, "w") as f:
+    f.write("This is a test log file." + log_file_name)
+
+  # Return the path to the log file.
+  return log_file_path
 
 drive = "D:"
 folder = "TestLogs"
