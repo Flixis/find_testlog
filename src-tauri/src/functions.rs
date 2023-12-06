817fe932ace09544f55b226d062b9102a92c3eb9
Tariq <tariq9853@gmail.com>
2023-12-06 21:48:46 +0100

    deps for windows fixed, version bumped

--- src-tauri/src/functions.rs
+++ src-tauri/src/functions.rs
@@ -13,6 +13,7 @@ required to removed windows console when launching GUI.
 Tauri by default does not support this feature.
 
 */
+#[cfg(target_os = "windows")]
 pub fn hide_windows_console(switch: bool) {
     unsafe {
         if switch {
