1f143b19de3d8a4c8d13449a37df284f0a59f544
Tariq <tariq9853@gmail.com>
2024-04-17 22:26:48 +0200

    Refactor: added fallback, added error handling

--- src-tauri/src/extractors.rs
+++ src-tauri/src/extractors.rs
@@ -1,5 +1,6 @@
 use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
 use indexmap::IndexMap;
+use regex::Regex;
 use std::fs::File;
 use std::io::{self, Read, Seek, SeekFrom};
 use std::path::Path;
@@ -11,12 +12,15 @@ Pattern matches on datetime and clnt, which is required to build a valid date-ti
 */
 /// Extracts date and time from filepath and parses it as chrono:naivedate
 pub fn extract_datetime_clnt_from_logpath(log_path: &str) -> (String, String) {
-    
     let path = Path::new(log_path);
-    let log_path = path.file_name().and_then(|f| f.to_str()).ok_or_else(|| {
-        log::error!("Failed to extract log filename from path");
-        "Error: Could not extract log filename"
-    }).unwrap();
+    let log_path = path
+        .file_name()
+        .and_then(|f| f.to_str())
+        .ok_or_else(|| {
+            log::error!("Failed to extract log filename from path");
+            "Error: Could not extract log filename"
+        })
+        .unwrap();
     let parts: Vec<&str> = log_path.split('_').collect();
 
     // Ensure the parts vector has at least the expected number of elements
@@ -25,10 +29,8 @@ pub fn extract_datetime_clnt_from_logpath(log_path: &str) -> (String, String) {
         let time_str = parts[1];
         let clnt = parts[2].to_string();
 
-        
         let default_date = NaiveDate::from_ymd_opt(1, 1, 1).unwrap_or_default();
         let default_time = NaiveTime::from_hms_opt(1, 1, 1).unwrap_or_default();
-
 
         // Parse date and time strings into chrono objects
         let date = match NaiveDate::parse_from_str(date_str, "%Y%m%d") {
@@ -86,11 +88,11 @@ Regex pattern matches on and returns something like:
 
 */
 /// Extracts information from log file using regex patterns:
-/// 
+///
 /// (\w+):\s*(.+)
-/// 
+///
 /// (\w+(?: \w+)*).*?id: (\d+); Release (\w+)
-/// 
+///
 /// b(PASS(?:ED)?|FAIL(?:ED)?)\b
 pub fn extract_info_from_log(
     filename: &str,
@@ -106,36 +108,51 @@ pub fn extract_info_from_log(
 
     println!("{:?}", &file);
 
-    //500 bytes is just a random number I picked, it could be moved higher if the need is there
-    let mut first_part = read_file_till_bytes(&file, bytes_to_read);
-    let mut second_part = read_file_till_bytes(&file, -bytes_to_read);
-    first_part = clean_up_string(&first_part);
-    second_part = clean_up_string(&second_part);
+    let mut first_part_of_file = match read_file_till_bytes(&file, bytes_to_read) {
+        Ok(content) => content,
+        Err(e) => {
+            log::warn!("File read operation failed: {}", e);
+            String::new()
+        }
+    };
 
+    let mut second_part_of_file = match read_file_till_bytes(&file, -bytes_to_read) {
+        Ok(content) => content,
+        Err(e) => {
+            log::warn!("File read operation failed: {}", e);
+            String::new()
+        }
+    };
 
+    // dbg!(&first_part_of_file);
+    dbg!(&second_part_of_file);
 
-    let mut cleaned_operation_headers = create_header_hashmap_from_headers_string(&first_part);
-    let cleaned_operation_status = create_status_hashmap_from_status_string(&second_part);
+    first_part_of_file = clean_up_string(&first_part_of_file);
+    second_part_of_file = clean_up_string(&second_part_of_file);
 
-    
-  
+    let mut cleaned_operation_headers =
+        create_header_hashmap_from_headers_string(&first_part_of_file);
+    let cleaned_operation_status = create_status_hashmap_from_status_string(&second_part_of_file);
+
     cleaned_operation_headers.extend(cleaned_operation_status);
     Ok(cleaned_operation_headers)
-
 }
 
-fn read_file_till_bytes(mut file: &File, bytes_to_read: i64) -> String{
-    
+fn read_file_till_bytes(mut file: &File, bytes_to_read: i64) -> Result<String, io::Error> {
     if bytes_to_read < 0 {
-        file.seek(SeekFrom::End(bytes_to_read)).unwrap();
+        //result unused because it returns cursor position which we dont need
+        let _file_seek_result = file
+            .seek(SeekFrom::End(bytes_to_read))
+            .or_else(|_| file.seek(SeekFrom::End(bytes_to_read / 2)));
     }
     let mut buffer = vec![0; bytes_to_read.abs() as usize];
-    let n = file.read(&mut buffer).unwrap();
-    
-    // Convert the buffer to a String, handling potential errors.
-    let text = String::from_utf8(buffer[..n].to_vec()).unwrap();
-    text
+    let bytes_extracted_from_file = file.read(&mut buffer).unwrap();
 
+    // Convert the buffer to a String, handling potential errors.
+    let text = String::from_utf8(buffer[..bytes_extracted_from_file].to_vec())
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Invalid UTF-8 sequence"));
+
+    text
 }
 
 fn clean_up_string(input: &str) -> String {
@@ -164,7 +181,10 @@ fn create_header_hashmap_from_headers_string(data: &String) -> IndexMap<String, 
                     for part in &parts[1..] {
                         if part.starts_with("(id:") {
                             let id = parts[2].to_string();
-                            hashmap.insert("id".to_string(), id);
+                            hashmap.insert(
+                                "id".to_string(),
+                                id.strip_suffix(";").unwrap_or_default().to_string(),
+                            );
                         } else if part.starts_with("Release") {
                             let release = parts[4..5].concat().to_string();
                             hashmap.insert("release".to_string(), release);
@@ -182,7 +202,7 @@ fn create_header_hashmap_from_headers_string(data: &String) -> IndexMap<String, 
     hashmap
 }
 
-fn create_status_hashmap_from_status_string(input: &str) -> IndexMap<String, String>{
+fn create_status_hashmap_from_status_string(input: &str) -> IndexMap<String, String> {
     let mut results = IndexMap::new();
 
     // Find the start of the "Test Results" section
@@ -203,7 +223,21 @@ fn create_status_hashmap_from_status_string(input: &str) -> IndexMap<String, Str
                 let result = parts[result_index].to_string();
 
                 // Insert the serial number and result into the HashMap
+                dbg!(&parts);
                 results.insert(serial_number, result);
+            }
+        }
+    } else {
+        log::warn!("Failed to parse PASS/FAIL state, using fallback");
+        let pass_or_fail_regex = Regex::new(r"\b(PASS(?:ED)?|FAIL(?:ED)?)\b").unwrap();
+
+        for line in input.lines() {
+            if let Some(caps) = pass_or_fail_regex.captures(&line) {
+                let key = "PASS_FAIL_STATUS".to_string();
+                let value = caps[1].to_string();
+                {
+                    results.insert(key.clone(), value);
+                }
             }
         }
     }
